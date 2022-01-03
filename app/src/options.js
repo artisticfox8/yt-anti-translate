abe792d3f6e5ef56f0778eac28b72697ba532f67
artisticfox8 <77014769+artisticfox8@users.noreply.github.com>
2022-01-03 18:59:04 +0100

    Update options.js to version 1.5

--- app/src/options.js
+++ app/src/options.js
@@ -1,4 +1,6 @@
 function saveOptions() {
+    
+
     chrome.storage.sync.get({
         disabled: false
     }, function (items) {
@@ -10,10 +12,34 @@ function saveOptions() {
             document.getElementById('status').innerText = disabled ? 'Disabled' : 'Enabled';
             document.getElementById('status').className = 'status ' + (disabled ? 'disabled' : 'enabled');
             document.getElementById('disable-button').className = disabled ? 'disabled' : 'enabled';
+            tellThePage();
+
         });
     });
 }
+function tellThePage(){
+    let message;
+    if(document.getElementById('status').innerText == "Enabled"){ //The value is set correctly
+        message = "Enable";
+    }else if(document.getElementById('status').innerText == "Disabled"){
+        message = "Disable";
+    }
+    browser.tabs.query({url:"*://*.youtube.com/*"}, function(tabs){
 
+        for (var x = 0; x < tabs.length; x++) {
+            //sending a message to content script https://stackoverflow.com/questions/14245334/sendmessage-from-extension-background-or-popup-to-content-script-doesnt-work
+            browser.tabs.sendMessage(tabs[x].id, message)
+            .then(response => { 
+              console.log("Message from the content script:",response);
+
+            })
+            .catch(e => {
+                console.log(e);
+            });
+        }
+        
+    });
+}
 function loadOptions() {
     chrome.storage.sync.get({
         disabled: false
